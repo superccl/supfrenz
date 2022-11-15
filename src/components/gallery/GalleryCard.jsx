c642b9737fa4d2f4fcb207477b0bd00e74bea3fa
Andy <chunlongandychan@yahoo.com.hk>
2022-11-15 23:08:57 +0800

    In Progress: Convert util.scss to util.css

--- src/components/gallery/GalleryCard.jsx
+++ src/components/gallery/GalleryCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../../styles/util.css'
 
 const GalleryCard = ({ img, serialNumber }) => {
   return (
-    <div className="w-full">
+    <div className="w-full wiggle-animation">
         <Link to={`/details/${serialNumber}`}>
             <img src={img} alt="image" className="w-full aspect-square object-cover rounded-lg" />
             <p className='text-center m-4 text-2xl'>Serial #{serialNumber}</p>
